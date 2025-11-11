21511ae217a77a8487e9a60cbbb63fa0d3afd5f7
kisstherain8677 <842658636@qq.com>
2025-11-12 01:29:58 +0800

    添加新的塔类型

---
+++ Assets/Scripts/MortarTower.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MortarTower : Tower
+{
+    [SerializeField, Range(0.5f, 2f)]
+    float shotPerSecond = 1f;
+
+    [SerializeField]
+    Transform mortar = default;
+
+    public override TowerType TowerType => TowerType.Mortar;
+}
