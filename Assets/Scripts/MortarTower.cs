d5f4f788362bfd2186f0ce4294c6b4c79ac64c71
zengweihao <zwh86771385@gmail.com>
2025-11-18 00:15:28 +0800

    发射速度和角度计算

--- Assets/Scripts/MortarTower.cs
+++ Assets/Scripts/MortarTower.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,5 +10,75 @@ public class MortarTower : Tower
     [SerializeField]
     Transform mortar = default;
 
+    //落点确定 速度大小影响发射角度
+    //如果速度过小 会无解 找这个最小值
+    //也就是一元二次方程 delta=0的时候
+    float launchSpeed;
+
+    private void Awake()
+    {
+        OnValidate();
+    }
+
+    private void OnValidate()
+    {
+        float x = targetingRange + 0.25001f;
+        float y = -mortar.position.y;
+        launchSpeed = Mathf.Sqrt(9.81f * (y + Mathf.Sqrt(x * x + y * y)));
+    }
+
+
     public override TowerType TowerType => TowerType.Mortar;
+
+    public override void GameUpdate()
+    {
+        base.GameUpdate();
+        Launch(new Vector3(3f, 0f, 0f));
+        Launch(new Vector3(0f, 0f, 1f));
+        Launch(new Vector3(1f, 0f, 1f));
+        Launch(new Vector3(3f, 0f, 1f));
+    }
+
+    public void Launch(Vector3 offset)
+    {
+        Vector3 launchPoint = mortar.position;
+        Vector3 targetPoint = launchPoint + offset;
+        targetPoint.y = 0f;
+
+        Vector2 dir;
+        dir.x = targetPoint.x-launchPoint.x;
+        dir.y = targetPoint.z-launchPoint.z;
+        float x = dir.magnitude;//向量长度
+        float y = -launchPoint.y;
+        dir /= x;//单位向量
+
+        float g = 9.81f;
+        float s = launchSpeed;//速度大小 斜方向
+        float s2 = s * s;
+
+        float r = s2 * s2 - g * (g * x * x + 2f * y * s2);
+        //物理问题确定落点 初始速度 求发射角度
+        float tanTheta = (s2 + Mathf.Sqrt(r)) / (g * x);
+        float cosTheta = Mathf.Cos(Mathf.Atan(tanTheta));
+        float sinTheta = cosTheta * tanTheta;
+
+        Vector3 prev = launchPoint, next;
+        for(int i = 1; i <= 10; i++)
+        {
+            float t = i / 10f;
+            float dx = s * cosTheta * t;
+            float dy = s * sinTheta * t - 0.5f * g * t * t;
+            next = launchPoint + new Vector3(dir.x * dx, dy, dir.y * dx);
+            Debug.DrawLine(prev, next, Color.blue);
+            prev = next;
+        }
+
+        Debug.DrawLine(launchPoint, targetPoint, Color.yellow);
+        Debug.DrawLine(
+            new Vector3(launchPoint.x,0.01f,launchPoint.z),
+            new Vector3(launchPoint.x+dir.x*x,0.01f,launchPoint.z+dir.y*x),
+            Color.white
+            );
+    }
+
 }
